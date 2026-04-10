9f08c2210e47c012f23bfbe949a51ebdad27c5f7
maejam <git.rejoice553@passmail.net>
2026-04-10 12:30:15 +0200

    docs(examples): update labels example

--- examples/03-labels.py
+++ examples/03-labels.py
@@ -5,20 +5,20 @@ from manim import *
 from manim_grid import Grid
 
 
-class Labels(Scene):
+class LabelsAndNumbers(Scene):
     def construct(self):
         # Give meaningful names with `row_labels` and `col_labels`
-        grid = Grid([1] * 5, [1] * 3, col_labels=["row_label", "left", "right"])
+        grid = Grid([1] * 5, [1] * 4, col_labels=["row\nnumber", "left", "center", "right"])
         self.add(grid)
         grid.lattice.set_stroke(opacity=1)
 
         # Add the labels to the grid as Text mobjects
-        grid.mobs[0, :] = grid.col_labels(font_size=12)
-        # row_labels was not provided => 1-based numeric labels have been auto-generated
-        grid.mobs[:, 0] = grid.row_labels(font_size=12, color=BLUE)
+        grid.mobs[0, :] = grid.col_labels(font_size=16)
+        # row_labels was not provided. We can still add row_numbers. start/stop/step are optional.
+        grid.mobs[1:, 0] = grid.row_numbers(start=1, stop=5, step=1, font_size=26, color=BLUE)
 
         # Labels can be used in-place of numeric indexes for more expressive indexing
-        grid.mobs["2", "right"] = Circle(radius=0.2)  # equivalent to grid.mobs[1, 2]
+        grid.mobs[1, "right"] = Circle(radius=0.2)  # equivalent to grid.mobs[1, 2]
         # Also works for slices and other complex indexes
-        grid.rects["2"::2].set_fill(WHITE, opacity=1)
+        grid.rects[:, "left"::2].set_fill(WHITE, opacity=0.2)
         grid.add(grid.mobs[:])
